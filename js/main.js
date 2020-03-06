9d6d4f563c467eff103283441e2d6ec994416870
davindersharma <davindersharma@users.noreply.github.com>
2020-03-06 14:16:08 -0500

    Complete

--- js/main.js
+++ js/main.js
@@ -18,4 +18,35 @@ $(document).ready(function() {
 
     })
 
+    // owl-carsol=====
+
+    $(".site-main .about-area .owl-carousel").owlCarousel({
+            loop: true,
+            autoplay: true,
+            dots: true,
+            responsive: {
+                0: {
+                    item: 1
+                },
+                544: {
+                    item: 2
+                }
+            }
+        })
+        //    nav bar===============
+
+    let nav_offset_top = $('.header_area').height() + 50;
+
+    function navbarFixed() {
+        if ($('header_area').length) {
+            $(window).scroll(function() {
+                let scroll = $(window).scrollTop();
+                if (scroll >= nav_offset_top) {
+                    $('.header_area .main-menu').addClass('navbar_fixed')
+                } else {
+                    $('.header_area .main-menu').removeClass('navbar_fixed')
+                }
+            })
+        }
+    }
 });
